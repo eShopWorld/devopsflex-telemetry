c7bf7e6f53a2dcb0fd544f8988a51201f46761a9
djfr <davidferreirarodrigues@gmail.com>
2017-10-10 16:52:39 +0100

    Added a single replay (multi)cast class

---
+++ src/DevOpsFlex.Telemetry/SingleReplayCast.cs
@@ -0,0 +1,58 @@
+﻿namespace DevOpsFlex.Telemetry
+{
+    using System;
+    using System.Reactive.Linq;
+    using System.Reactive.Subjects;
+
+    /// <summary>
+    /// Replays a single time from an original <see cref="IObservable{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the original stream.</typeparam>
+    public class SingleReplayCast<T> : IDisposable
+    {
+        internal ReplaySubject<T> Replay = new ReplaySubject<T>();
+
+        internal IDisposable ReplayConnection;
+
+        internal IDisposable ReplaySubscription;
+
+        internal readonly object Gate = new object();
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="SingleReplayCast{T}"/>
+        /// </summary>
+        /// <param name="origin">The original stream that we want to replay a single time.</param>
+        public SingleReplayCast(IObservable<T> origin)
+        {
+            var connection = origin.Multicast(Replay);
+            ReplayConnection = connection.Connect();
+        }
+
+        /// <summary>
+        /// Subscribes to a single replay buffer.
+        ///     This will check if there's already a single active subscription, and if it is returns the existing one instead.
+        /// </summary>
+        /// <param name="action">The <see cref="Action{T}"/> we want to perform during the subscription.</param>
+        /// <returns>The subscription <see cref="IDisposable"/>.</returns>
+        public IDisposable Subscribe(Action<T> action)
+        {
+            lock (Gate)
+            {
+                if (ReplaySubscription != null)
+                {
+                    Replay.OnCompleted();
+                    ReplaySubscription = Replay.Subscribe(action, Dispose);
+                }
+
+                return ReplaySubscription;
+            }
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            ReplayConnection?.Dispose();
+            Replay?.Dispose();
+        }
+    }
+}
