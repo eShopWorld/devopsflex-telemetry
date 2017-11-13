59a13656c02dbb4d226ef514fe499ee7ed273d42
Ruaidhri Mc Donnell <ruaidhri.mcdonnell@gmail.com>
2017-11-13 10:36:25 +0000

    add telemetry processor to add role name to AI messages

---
+++ src/DevOpsFlex.Telemetry/RoleNameSetter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace DevOpsFlex.Telemetry
+{
+    /// <summary>
+    /// Sets the 'cloud_RoleName' role name attribute on a AI telemetry item
+    /// </summary>
+    /// <remarks>See https://docs.microsoft.com/en-us/azure/application-insights/app-insights-monitor-multi-role-apps </remarks>
+    /// <example>
+    /// &lt;TelemetryProcessors&gt;
+    /// &lt;!-- Insert this in ApplicationInsights.config --&gt;
+    ///     &lt;Add Type="DevOpsFlex.Telemetry.RoleNameSetter, DevOpsFlex.Telemetry"&gt;
+    ///         &lt;RoleName>MyApplication&lt;/RoleName&gt;
+    ///     &lt;/Add&gt;
+    /// &lt;/TelemetryProcessors&gt;
+    /// 
+    /// // OR 
+    /// // alternatively, you can initialize the filter in code. In an initialization class (Global.asax.cs) - insert the processor into the chain:
+    /// 
+    /// var builder = TelemetryConfiguration.Active.TelemetryProcessorChainBuilder;
+    /// builder.Use((next) => new RoleNameSetter(next) { RoleName = "MyApplication" });
+    /// builder.Build();
+    /// </example>
+    public class RoleNameSetter : ITelemetryProcessor
+    {
+        public string RoleName { get; set; }
+        private ITelemetryProcessor Next { get; }
+
+        public RoleNameSetter([NotNull] ITelemetryProcessor next)
+        {
+            this.Next = next ?? throw new ArgumentNullException(nameof(next));
+            RoleName = System.Reflection.Assembly.GetEntryAssembly()?.FullName; // this will not resolve for an ASP.NET application.
+        }
+
+        public void Process(ITelemetry item)
+        {
+            if (item != null)
+            {
+                if (string.IsNullOrWhiteSpace(item?.Context?.Cloud.RoleName)) item.Context.Cloud.RoleName = RoleName;
+            }
+
+            this.Next.Process(item);
+        }
+    }
+}
