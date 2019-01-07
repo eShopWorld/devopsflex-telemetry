83a4a853dc072fb5d0ff78896b9341b96e258146
djfr <davidferreirarodrigues@gmail.com>
2019-01-07 12:32:31 +0000

    Declared friendship with the snkrs control tower actor project

--- src/Eshopworld.Telemetry/AssemblyInfo.cs
+++ src/Eshopworld.Telemetry/AssemblyInfo.cs
@@ -2,3 +2,5 @@
 
 [assembly: InternalsVisibleTo("Eshopworld.Telemetry.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+[assembly: InternalsVisibleTo("Nike.Snkrs.WatchDogActor")]
