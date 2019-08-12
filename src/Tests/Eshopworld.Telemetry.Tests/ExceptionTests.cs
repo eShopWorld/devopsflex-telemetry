2b59d3af77f5a93783550f3ea15e8957488cddd0
David Guerin <david.guerin@developershelf.com>
2019-08-12 23:07:16 +0100

    Setting it up

---
+++ src/Tests/Eshopworld.Telemetry.Tests/ExceptionTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Eshopworld.Core;
+using Eshopworld.Tests.Core;
+using Xunit;
+
+namespace Eshopworld.Telemetry.Tests
+{
+    public class ExceptionTests
+    {
+        private static readonly string DevKey = Environment.GetEnvironmentVariable("devai", EnvironmentVariableTarget.User);
+
+        [Fact]
+        [IsLayer0]
+        public void CustomExceptionTest()
+        {
+            var customException = new CustomException
+            {
+                MyCustomDateTime = DateTime.UtcNow,
+                MyCustomProperty = "Hello World"
+            };
+
+            var bigBrother = new BigBrother(DevKey, DevKey).DeveloperMode();
+
+            bigBrother.Publish(customException.ToExceptionEvent());
+            bigBrother.Flush();
+        }
+
+        public class CustomException : Exception
+        {
+            public string MyCustomProperty { get; set; }
+
+            public DateTime MyCustomDateTime { get; set; }
+        }
+    }
+}
