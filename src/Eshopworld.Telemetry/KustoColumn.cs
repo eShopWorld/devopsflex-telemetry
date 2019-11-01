b4f6ea50bbc40b34506bcdad4ccb11f874480685
Hrvoje Hudo <hhudoletnjak@eshopworld.com>
2019-11-01 18:11:41 +0000

    WIP, schema changes fixes

--- src/Eshopworld.Telemetry/KustoColumn.cs
+++ src/Eshopworld.Telemetry/KustoColumn.cs
@@ -1,25 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using Kusto.Cloud.Platform.Utils;
 using Kusto.Data.Common;
 
 namespace Eshopworld.Telemetry
 {
     internal class KustoColumn : IEquatable<KustoColumn>
     {
+        private static Dictionary<string, string> NameMaps = new Dictionary<string, string>
+        {
+            { "i32", "int32" },
+            { "stringbuffer", "string" }
+        };
+            
         internal KustoColumn(string name, string cslType)
         {
             Name = name;
-            CslType = CslType.FromCslType(cslType);
+
+            cslType = cslType.ToLower();
+            CslType = CslType.FromCslType(NameMaps.GetOrDefault(cslType, cslType));
+            ClrType = CslType.GetCorrespondingClrType();
         }
 
         internal KustoColumn(string name, Type type)
         {
             Name = name;
             CslType = CslType.FromClrType(type);
+            ClrType = type;
         }
 
         public string Name { get; }
 
         public CslType CslType { get; }
+
+        public Type ClrType { get; set; }
 
         public bool Equals(KustoColumn other)
         {
