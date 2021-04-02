940f2cc68fdb95f546158e4dfad364917738b34b
Mayank Goyel <mgoyel@eshopworld.com>
2021-04-02 14:19:34 +0100

    Revert "Fixed the issue of broken registration by the last version change"
    This reverts commit 436eab0e773d5d44459cb22865ca422a09fbc786.

--- src/Eshopworld.Telemetry/Processors/SuccessfulProbeFilterCriteria.cs
+++ src/Eshopworld.Telemetry/Processors/SuccessfulProbeFilterCriteria.cs
@@ -13,19 +13,11 @@ namespace Eshopworld.Telemetry.Processors
     /// </summary>
     public class SuccessfulProbeFilterCriteria : ITelemetryFilterCriteria
     {
-        private readonly string[] _methodNames = {"GET", "HEAD"};
+        private readonly string[] _methodNames =  {"GET", "HEAD"};
 
-        private readonly string[] _defaultProbePaths = {"probe/", "/probe"};
+        private readonly string[] _defaultProbePaths = { "probe/", "/probe" };
 
         private readonly List<string> _requestNamesToMatch;
-
-        /// <summary>
-        /// Creates an instance of <see cref="SuccessfulProbeFilterCriteria"/>
-        /// </summary>
-        /// <param name="healthCheckPath"></param>
-        public SuccessfulProbeFilterCriteria(string healthCheckPath) : this(new[] {healthCheckPath})
-        {
-        }
 
         /// <summary>
         /// Creates an instance of <see cref="SuccessfulProbeFilterCriteria"/>
@@ -41,7 +33,7 @@ namespace Eshopworld.Telemetry.Processors
 
             _requestNamesToMatch.AddRange(BuildMatches(_defaultProbePaths));
 
-            if (healthChecksPaths != null && healthChecksPaths.Any())
+            if (healthChecksPaths!=null && healthChecksPaths.Any())
             {
                 _requestNamesToMatch.AddRange(BuildMatches(healthChecksPaths));
             }
